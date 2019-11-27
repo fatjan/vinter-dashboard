20de377237cb933ac5eca9962e76ebe031a6c602
Fikri Humam Manar Amri <fikrihumamma@Fikri-H-Air.local>
2019-11-27 16:47:21 +0700

    skeleton done

---
+++ pages/pilih-posisi.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-row class="banner">
+      <v-container>
+        <v-row>
+          <v-col cols="12" xs="12" sm="12" md="10" lg="10" xl="10">
+            <h4>
+              BukaToko
+            </h4>
+            <p>
+              BukaToko adalah startup e-commerce pertama di Bojonegoro. Start-up
+              ini membantu IKM di Bojonegoro memasarkan produk mereka.
+            </p>
+          </v-col>
+          <v-col cols="12" xs="12" sm="12" md="2" lg="2" xl="2">
+            Posisi Tersedia
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-row>
+    <v-container>
+      <v-row>
+        <v-col cols="12" xs="12" sm="12" md="3" lg="3" xl="3">
+          <h5>
+            <img src="" alt="" />
+            E-Commerce
+          </h5>
+        </v-col>
+        <v-col cols="12" xs="12" sm="12" md="9" lg="9" xl="9">
+          Posisi Tersedia
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'OtherPageLayout'
+}
+</script>
+<style>
+.banner {
+  background-color: #c1f5e4;
+  margin-top: 65px;
+}
+</style>
