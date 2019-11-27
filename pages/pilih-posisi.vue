08a95e58fbea4959fc238a610ff44ebb461c17c9
Fikri Humam Manar Amri <fikrihumamma@Fikri-H-Air.local>
2019-11-27 17:09:37 +0700

    banner done

--- pages/pilih-posisi.vue
+++ pages/pilih-posisi.vue
@@ -3,17 +3,19 @@
     <v-row class="banner">
       <v-container>
         <v-row>
-          <v-col cols="12" xs="12" sm="12" md="10" lg="10" xl="10">
-            <h4>
-              BukaToko
-            </h4>
+          <v-col cols="12" xs="12" sm="12" md="11" lg="11" xl="11">
+            <h3>
+              <strong>
+                BukaToko
+              </strong>
+            </h3>
             <p>
               BukaToko adalah startup e-commerce pertama di Bojonegoro. Start-up
               ini membantu IKM di Bojonegoro memasarkan produk mereka.
             </p>
           </v-col>
-          <v-col cols="12" xs="12" sm="12" md="2" lg="2" xl="2">
-            Posisi Tersedia
+          <v-col cols="12" xs="12" sm="12" md="1" lg="1" xl="1">
+            <img src="~/assets/img/dot.png" alt="" class="dot" />
           </v-col>
         </v-row>
       </v-container>
@@ -42,5 +44,17 @@ export default {
 .banner {
   background-color: #c1f5e4;
   margin-top: 65px;
+  height: 150px;
+  padding: 5px 0px;
+}
+.dot {
+  padding-top: 15px;
+}
+h3,
+p {
+  color: #23573e;
+}
+p {
+  font-size: 18px;
 }
 </style>
